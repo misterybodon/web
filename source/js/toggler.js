ab13f2a3ca1bad72af21de2b115d6cba9e620a2f
Santiago Miranda <santimirandarp@gmail.com>
2020-03-17 01:06:35 +0000

    Add a new animation on the image

--- source/js/toggler.js
+++ source/js/toggler.js
@@ -2,6 +2,7 @@ const navBar = document.getElementById("navbar__ul")
 const lines = document.getElementById("hamburger").children
 const cardH2Chevron=document.getElementById("card__h2chevron");
 const wrapper=document.getElementById("card__wrapper")
+const rotateCard=document.getElementById("rotating__card")
 function clickBurger() {
     navBar.classList.toggle("trigger__navbar")
     lines[0].classList.toggle("hamburger__line1")
@@ -13,4 +14,6 @@ function showCard(){
 wrapper.classList.toggle("card__wrapper--fullHeight");
 cardH2Chevron.classList.toggle("card__h2chevron--close");
 }
-
+function rotateCard(){
+    rotateCard.classList.toggle("rotating__card")
+}
