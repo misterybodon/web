ae6c7d7a0a2a343dd5d3080a312cfae781950ea1
santi <santimir.me@gmail.com>
2020-04-29 12:12:16 +0100

    Animation to header, opacity.

--- dist/js/toggler.js
+++ dist/js/toggler.js
@@ -3,8 +3,10 @@ const lines = document.getElementById("hamburger").children
 const cardH2Chevron=document.getElementById("card__h2chevron");
 const wrapper=document.getElementById("card__wrapper")
 const rotateImage=document.getElementById("card__rotateImage")
+
 function clickBurger() {
     navBar.classList.toggle("trigger__navbar")
+    toggleH1()
     lines[0].classList.toggle("hamburger__line1")
     lines[1].classList.toggle("hamburger__line2")
     lines[2].classList.toggle("hamburger__line3")
@@ -16,4 +18,10 @@ cardH2Chevron.classList.toggle("card__h2chevron--close")
 }
 function rotateCardImage(){
     rotateImage.classList.toggle("card__rotateImage")
+}
+
+
+function toggleH1() {
+const h1 = document.getElementById("showcase__h1");
+h1.classList.toggle("h1Vanisher");
 }
