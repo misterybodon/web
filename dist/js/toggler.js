d16d60a166a31890a6c8e0514b22c8fff45d7da1
santi <santimir.me@gmail.com>
2020-05-13 13:04:24 +0100

    Finished main page.

--- dist/js/toggler.js
+++ dist/js/toggler.js
@@ -1,16 +1,19 @@
-const navBar = document.getElementById("navbar__ul")
-const lines = document.getElementById("hamburger").children
+const navBar = document.getElementById("navbar__ul");
+const lines = document.getElementById("hamburger").children;
 const cardH2Chevron=document.getElementById("card__h2chevron");
-const wrapper=document.getElementById("card__wrapper")
-const rotateImage=document.getElementById("card__rotateImage")
-
+const wrapper=document.getElementById("card__wrapper");
+const rotateImage=document.getElementById("card__rotateImage");
+const h1 = document.getElementById("showcase__h1");
+const hamburger=document.getElementById("hamburger");
 function clickBurger() {
     navBar.classList.toggle("trigger__navbar")
-    toggleH1()
     lines[0].classList.toggle("hamburger__line1")
     lines[1].classList.toggle("hamburger__line2")
     lines[2].classList.toggle("hamburger__line3")
+    h1.classList.toggle("h1Vanisher");
   }
+
+hamburger.onclick=clickBurger;
 
 function showCard(){
 wrapper.classList.toggle("card__wrapper--fullHeight")
@@ -21,7 +24,3 @@ function rotateCardImage(){
 }
 
 
-function toggleH1() {
-const h1 = document.getElementById("showcase__h1");
-h1.classList.toggle("h1Vanisher");
-}
