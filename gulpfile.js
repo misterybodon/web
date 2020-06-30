9e057cb705b365e3371aa0fc49303c8222f85eb7
Mister Nobody <misterybodon@gmail.com>
2020-06-30 14:19:41 +0100

    Updated gulpfile for better readability

--- gulpfile.js
+++ gulpfile.js
@@ -9,86 +9,94 @@ sass.compiler = require('sass')
 const imagemin = require('gulp-imagemin')
 const clean = require('gulp-clean')
 const rename = require('gulp-rename')
-//path to file
-const files = {
-	mainHTML:'source/**/*.html',
-	mainCss: 'dist/css',
-	mainScss: 'source/sass/index.scss',
-	notmainScss:'!source/sass/index.scss',
-	//post stands for posts, for each blog post, not for post processing.
-	postScss:'source/**/*.scss',
-	dist:'dist/',
-	source:'source/',
-	jsSource:'source/js/*',
-	jsDist:'dist/js/',
-	imgsrcfile:'source/images/*',
-	imgSource:'source/images/',
-	imgDest:'dist/images'
+
+//project files and folders
+const project = {
+    inFile:{
+            html:'source/**/*.html',
+            mainScss: 'source/sass/index.scss',
+            notMainScss:'!source/sass/index.scss',
+            scss:'source/**/*.scss',
+            js:'source/js/*',
+            img:'source/images/*'
+    },
+    outDir:{
+            js:'dist/js/',
+            css: 'dist/css',
+            img:'dist/images'
+    },
+    inDir:{
+        img:'source/images/'
+    },
+    dist:'dist/',
+    source:'source/',
 }
-//copy files to dist folder.
+//copy project to dist folder.
 const copyTask = parallel(cpJs, cpHtml, scssTask);
 
 function cpHtml() {
-	return src(files.mainHTML)
-		.pipe(dest(files.dist))
+    //copy html files from source to dist.
+    return src(project.inFile.html)
+        .pipe(dest(project.dist))
 }
 
 function cpJs() {
-	return src(`${files.jsSource}`)
-		.pipe(dest(`${files.jsDist}`))
+    return src(`${project.inFile.js}`)
+        .pipe(dest(`${project.outDir.js}`))
 }
 
-//..compile the scss files to compressed, prefixed css files and copy them to dist.
+//..compile the scss project to compressed, prefixed css project and copy them to dist.
 function scssTask(cb) {
-	mainScss();
-	postScss();
-	cb()
+    mainScss();
+    scss();
+    cb()
 }
 function mainScss(){
-	return src(files.mainScss)
-		.pipe(sourcemaps.init())
-		.pipe(sass({ fiber: Fiber, outputStyle: 'compressed' }))
-		.pipe(postcss([autoprefixer()]))
-		.pipe(sourcemaps.write())
-		.pipe(dest(files.dist))
+    return src(project.inFile.mainScss)
+        .pipe(sourcemaps.init())
+        .pipe(sass({ fiber: Fiber, outputStyle: 'compressed' }))
+        .pipe(postcss([autoprefixer()]))
+        .pipe(sourcemaps.write())
+        .pipe(dest(project.dist))
+    //just place the compiled css file under dist.
 }
 //post stands for posts, for each blog post, not for post processing.
-function postScss(){
-	return src([files.postScss,files.notmainScss])
-		.pipe(sourcemaps.init())
-		.pipe(sass({ fiber: Fiber, outputStyle: 'compressed' }))
-		.pipe(postcss([autoprefixer()]))
-		.pipe(sourcemaps.write())
-		.pipe(dest(files.dist))
+function scss(){
+    return src([project.inFile.scss,project.inFile.notMainScss])
+        .pipe(sourcemaps.init())
+        .pipe(sass({ fiber: Fiber, outputStyle: 'compressed' }))
+        .pipe(postcss([autoprefixer()]))
+        .pipe(sourcemaps.write())
+        .pipe(dest(project.dist))
 }
-//only minified files that have not been minified before.
+//only minified project that have not been minified before.
 function minifyImgs(){
-	return src([files.imgsrcfile, `!${files.imgSource}*-min*`])
-		.pipe(imagemin([
-			imagemin.gifsicle({interlaced: true}),
-			imagemin.mozjpeg({quality: 75, progressive: true}),
-			imagemin.optipng({optimizationLevel: 5}),
-			imagemin.svgo({
-				plugins: [
-					{removeViewBox: true},
-					{cleanupIDs: false}
-				]
-			})
-		]))
-		.pipe(clean())
-		.pipe(rename(function(path){path.basename += '-min'}))
-		.pipe(dest(files.imgSource))
-		.pipe(dest(files.imgDest))
+    return src([project.inFile.img, `!${project.inFile.img}*-min*`])
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.mozjpeg({quality: 75, progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        .pipe(clean())
+        .pipe(rename(function(path){path.basename += '-min'}))
+        .pipe(dest(project.outDir.img))
+        .pipe(dest(project.outDir.img))
 }
 function watchTask() {
-	return watch(
-		[files.mainScss,
-			files.postScss,
-			files.mainHTML, 
-			files.jsSource,
-			files.imgSource], 
-		parallel(copyTask, minifyImgs)
-	)}
+    return watch(
+        [project.inFile.mainScss,
+            project.inFile.scss,
+            project.inFile.html, 
+            project.inFile.js,
+            project.inFile.img], 
+        parallel(copyTask, minifyImgs)
+    )}
 //to run when running 'gulp' i.e the default task
 exports.onlyhtml = cpHtml 
 exports.noImage = copyTask;
