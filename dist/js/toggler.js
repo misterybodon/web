966959fb007b908ef0191ad86658f3d93398dc1f
Mister Nobody <misterybodon@gmail.com>
2020-06-30 20:58:29 +0100

    w3c validation. clean burger code.

--- dist/js/toggler.js
+++ dist/js/toggler.js
@@ -1,26 +1,22 @@
 const navBar = document.getElementById("navbar__ul");
 const lines = document.getElementById("hamburger").children;
-const cardH2Chevron=document.getElementById("card__h2chevron");
-const wrapper=document.getElementById("card__wrapper");
-const rotateImage=document.getElementById("card__rotateImage");
 const h1 = document.getElementById("showcase__h1");
 const hamburger=document.getElementById("hamburger");
-function clickBurger() {
+
+const clickBurger = () => {
     navBar.classList.toggle("trigger__navbar")
     lines[0].classList.toggle("hamburger__line1")
     lines[1].classList.toggle("hamburger__line2")
     lines[2].classList.toggle("hamburger__line3")
     h1.classList.toggle("h1Vanisher");
-  }
-
-hamburger.onclick=clickBurger;
-
-function showCard(){
-wrapper.classList.toggle("card__wrapper--fullHeight")
-cardH2Chevron.classList.toggle("card__h2chevron--close")
 }
-function rotateCardImage(){
-    rotateImage.classList.toggle("card__rotateImage")
-}
+hamburger.onclick = clickBurger
+//// function clickBurger() {
+//    navBar.classList.toggle("trigger__navbar")
+//    lines[0].classList.toggle("hamburger__line1")
+//    lines[1].classList.toggle("hamburger__line2")
+//    lines[2].classList.toggle("hamburger__line3")
+//    h1.classList.toggle("h1Vanisher");
+//  }
 
 
