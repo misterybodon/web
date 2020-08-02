da2f613c1deb66d4acfa64dcccb4c5cad200bbf9
Mister Nobody <misterybodon@gmail.com>
2020-08-02 01:25:49 +0100

    Improved code for updating posts (was silly).

--- source/js/update.js
+++ source/js/update.js
@@ -14,20 +14,29 @@ const posts = [
         title:"Mr. Nobody: Pink Floyd Cover", 
         brief:"Guitar cover of Wish You Were Here By Mister Nobody",
         publishIn:"May 24th, 2020", 
-        folder:"post2" //this isn't strictly necessary.
+        folder:"post2" 
     }, {
         title:"Mr. Nobody: Thoughts on Web Dev", 
         brief:"Let's tell the story from the looser point of view. And Mr. Nobody comes in.",
         publishIn:"May 16th, 2020", 
-        folder:"post1" //this isn't strictly necessary.
+        folder:"post1"
     }
-        
-    ]
+] //last posts first in the object.
 
-//last posts first in the object.
-
+const metaProps = {
+    page:{
+        description:'meta[name="description"]'
+    },
+    og:{
+        title:'meta[property="og:title"]', 
+        description:'meta[property="og:description"]', 
+        image:'meta[property="og:image"]'
+    },
+    twitter:{
+        title:'meta[name="twitter:title"]'
+    }
+}
 const updatePosts = (posts) => {    
-    //ul is a general name but will be function scoped.
     let ul = document.getElementsByClassName("newposts")[0].getElementsByTagName("ul")[0]
     ul.innerHTML=""
     let setStartDir=".."
@@ -36,7 +45,7 @@ const updatePosts = (posts) => {
     }
     for (let i=0; i<5 && i<posts.length; i++)
     {       
-    ul.innerHTML += `
+        ul.innerHTML += `
     <li><a href="${setStartDir}/post${posts.length - i}/">
     ${posts[i].publishIn}: ${posts[i].title}
     </a></li>\n
@@ -44,39 +53,23 @@ const updatePosts = (posts) => {
     }
     return 1
 }
-
-const setCurrentPostMetaTags = (posts) => {
-    let metaProps = {
-        page:{
-            description:'meta[name="description"]'
-        },
-        og:{
-            title:'meta[property="og:title"]', 
-            description:'meta[property="og:description"]', 
-            image:'meta[property="og:image"]'
-        },
-        twitter:{
-            title:'meta[name="twitter:title"]'
-        }
+const setMeta=(filename, metaProps, posts)=>{
+    const finder=/post\d+/;
+    const post = filename.match(finder) 
+    if(post){
+        const findnumber = /\d+/;
+        const number = String(post).match(findnumber)
+        const postData = posts[posts.length - number]
+        const {title, brief} = postData
+        const {og, page, twitter:tw} = metaProps
+        document.querySelector(page.description).setAttribute("content", `${title}`)
+        document.querySelector(og.title).setAttribute("content", `${title}`)
+        document.querySelector(og.description).setAttribute("content", `${brief}`)
+        document.querySelector(og.image).setAttribute( "content", "../images/ben-min.jpg")
+        document.querySelector(tw.title).setAttribute("content", `${title}`)
     }
-for(let i = 0; i<posts.length; i++){
-if(window.location.pathname.endsWith(`post${posts.length - i}`))
-    {
-        document.querySelector(metaProps.page.description).setAttribute("content", `${posts[i].title}`)
-        console.log("updated")
-        document.querySelector(metaProps.og.title).setAttribute("content", `${posts[i].title}`)
-        console.log("updated")
-        document.querySelector(metaProps.og.description).setAttribute("content", `${posts[i].brief}`)
-        console.log("updated")
-        document.querySelector(metaProps.og.image).setAttribute( "content", "../images/cartoon-min.jpg")
-        console.log("updated")
-        document.querySelector(metaProps.twitter.title).setAttribute("content", `${posts[i].title}`)
-        console.log("updated")
-        return 1
-    }
-}
-return -1
 }
 
 updatePosts(posts)
-setCurrentPostMetaTags(posts)
+
+setMeta(window.location.pathname, metaProps, posts)
